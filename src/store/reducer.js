090e024fc99943edbced9cf5bf7560acb61aea92
Andrew Lara <andrew.lara@mutualmobile.com>
2019-07-06 23:03:48 -0500

    Removed redux form

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import authReducer from '../modules/authentication';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  form: formReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer
 });
