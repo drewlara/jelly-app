5d2804a4e767cbfc5fc88355112181576ba6c721
Andrew Lara <andrew.lara@mutualmobile.com>
2019-07-16 22:07:09 -0500

    Added form

---
+++ src/form/validation.js
@@ -0,0 +1,13 @@
+function isDirty(value) {
+  return value || value === 0;
+}
+
+export function required(requiredFields, values) {
+  return requiredFields.reduce(
+    (fields, field) => ({
+      ...fields,
+      ...(isDirty(values[field]) ? undefined : { [field]: 'Required' }),
+    }),
+    {},
+  );
+}
